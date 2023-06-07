3487320d6e07fdcefeb367a4e893040b75757a8f
lee-orr <lee-orr@users.noreply.github.com>
2023-06-07 11:32:47 -0400

    add ui asset loader
    Signed-off-by: lee-orr <lee-orr@users.noreply.github.com>

--- src/lib.rs
+++ src/lib.rs
@@ -2,12 +2,15 @@ pub mod ui_asset;
 
 use bevy::prelude::*;
 
+use bevy_common_assets::xml::XmlAssetPlugin;
 use bevy_ecss::EcssPlugin;
+use ui_asset::UiNode;
 
 pub struct EcsUiPlugin;
 
 impl Plugin for EcsUiPlugin {
     fn build(&self, app: &mut App) {
-        app.add_plugin(EcssPlugin::default());
+        app.add_plugin(EcssPlugin::default())
+            .add_plugin(XmlAssetPlugin::<UiNode>::new(&["buml"]));
     }
 }
